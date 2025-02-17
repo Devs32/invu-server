fbffb33bf3a76f14939db5329f3d978a312b20c9
jparangdev <jparangdev@gmail.com>
2025-02-17 13:59:12 +0900

    test(boot): add test for boot

--- src/main/java/kr/co/devs32/TestController.java
+++ src/main/java/kr/co/devs32/TestController.java
@@ -1,12 +1,21 @@
 package kr.co.devs32;
 
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping(value = "/hello")
+
+    public static final String DEFAULT_MESSAGE = "hello INVU";
+
+    @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return getHello();
+    }
+
+    private String getHello() {
+        return DEFAULT_MESSAGE + LocalDateTime.now();
     }
 }
