b421eae881e706069f63a9a8ee4bfe1f4aeb2ba5
jhpark <pjhwin1@naver.com>
2025-02-25 22:58:28 +0900

    INVU-15 - swaggerConfig 테스트코드 작성

---
+++ src/test/java/kr/co/devs32/web/config/SwaggerConfigTest.java
@@ -0,0 +1,25 @@
+package kr.co.devs32.web.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class SwaggerConfigTest {
+
+    @Autowired
+    private OpenAPI openAPI;
+    @Test
+    void testCustomOpenAPI() {
+        assertNotNull(openAPI);
+
+        Info info = openAPI.getInfo();
+        assertNotNull(info);
+        assertEquals("API 문서", info.getTitle());
+        assertEquals("1.0", info.getVersion());
+        assertEquals("Spring Boot + Swagger 연동 예제", info.getDescription());
+    }
+}
