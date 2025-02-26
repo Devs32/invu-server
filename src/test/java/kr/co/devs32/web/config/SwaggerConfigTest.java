91336d7693090d79fe048224d39e969533e728a9
jhpark <pjhwin1@naver.com>
2025-02-26 21:54:49 +0900

    INVU-15 - SwaggerConfigTest PR 오류 수정

--- src/test/java/kr/co/devs32/web/config/SwaggerConfigTest.java
+++ src/test/java/kr/co/devs32/web/config/SwaggerConfigTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+@SpringBootTest(classes = SwaggerConfig.class)
 class SwaggerConfigTest {
 
     @Autowired
