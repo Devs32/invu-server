e6720ca5d4c3ff876f75fe5542562e57dec3989f
jhpark <pjhwin1@naver.com>
2025-03-08 17:11:21 +0900

    INVU-14 - 테스트 코드 작성2

---
+++ src/test/java/kr/co/devs32/web/domain/InvitationTest.java
@@ -0,0 +1,32 @@
+package kr.co.devs32.web.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvitationTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getInvuJson() {
+    }
+
+    @Test
+    void getCode() {
+    }
+
+    @Test
+    void getCreatedAt() {
+    }
+
+    @Test
+    void getUpdatedAt() {
+    }
+}
